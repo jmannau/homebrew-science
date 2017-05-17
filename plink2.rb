11114c148a6d3563200138070afb34058c4499e1
James Manners <james@manners.net.au>
2017-05-17 22:31:19 +1000

    Added devel option to install latest stable development release as an option to the tagged stable build

--- plink2.rb
+++ plink2.rb
@@ -1,17 +1,25 @@
 class Plink2 < Formula
   desc "Free, open-source whole genome association analysis toolset."
   homepage "https://www.cog-genomics.org/plink2"
-  url "https://github.com/chrchang/plink-ng/archive/v1.90b4.tar.gz"
-  version "1.90b4"
   # doi "10.1186/s13742-015-0047-8"
   # tag "bioinformatics"
-  sha256 "2ab909fc6c04062be56be86cf00ad6b3927316a34ae4ceb5fc4c250074cb3d26"
+
+  stable do
+    version "1.90b4"
+    url "https://github.com/chrchang/plink-ng/archive/v1.90b4.tar.gz"
+    sha256 "2ab909fc6c04062be56be86cf00ad6b3927316a34ae4ceb5fc4c250074cb3d26"
+  end
 
   bottle do
     cellar :any
     sha256 "61fba0c9016731531eaab1e88f1fc01d0ccbf41119c084e257e3f999fe8be0ad" => :sierra
     sha256 "87ef1226f0fd076e0683e2b30c006489cb76f2a44b241949cd44beec549a3126" => :el_capitan
     sha256 "540b6a0f57c322040f1338655456a32c7971a3320fdc17d09fba582930c5722e" => :yosemite
+  end
+
+  devel do
+    version "1.90b4.3"
+    url "https://github.com/chrchang/plink-ng.git", :revision => "49bcb9168478aad0ae047055f52ebbd9593392f3"
   end
 
   depends_on :fortran
