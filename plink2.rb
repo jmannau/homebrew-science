633232f89d6caabb654d40927f0bcfeb8e289a6b
James Manners <james@manners.net.au>
2017-05-19 09:44:29 +1000

    Updated plink formula to include 1.90b4 as a stable build and 1.90b43 as a development build

--- plink2.rb
+++ plink2.rb
@@ -1,15 +1,14 @@
 class Plink2 < Formula
-  url "https://github.com/chrchang/plink-ng/archive/v1.90b3.tar.gz"
-  version "1.90b3"
+  desc "Free, open-source whole genome association analysis toolset."
+  homepage "https://www.cog-genomics.org/plink2"
   # doi "10.1186/s13742-015-0047-8"
   # tag "bioinformatics"
-  homepage "https://www.cog-genomics.org/plink2"
-  sha256 "2f4afc193c288b13af4410e4587358ee0a6f76ed7c98dd77ca1317aac28adf0d"
-  revision 1
 
-  depends_on :fortran
-  depends_on "openblas" => :optional
-  depends_on "zlib"
+  stable do
+    version "1.90b4"
+    url "https://github.com/chrchang/plink-ng/archive/v1.90b4.tar.gz"
+    sha256 "2ab909fc6c04062be56be86cf00ad6b3927316a34ae4ceb5fc4c250074cb3d26"
+  end
 
   bottle do
     cellar :any
@@ -18,15 +17,26 @@ class Plink2 < Formula
     sha256 "540b6a0f57c322040f1338655456a32c7971a3320fdc17d09fba582930c5722e" => :yosemite
   end
 
+  devel do
+    version "1.90b4.3"
+    url "https://github.com/chrchang/plink-ng.git", :revision => "49bcb9168478aad0ae047055f52ebbd9593392f3"
+  end
+
+  depends_on :fortran
+  depends_on "openblas" => :optional
+  depends_on "zlib"
+
   def install
-    mv "Makefile.std", "Makefile"
-    ln_s Formula["zlib"].opt_include, "zlib-1.2.8"
-    cflags = "-Wall -O2 -flax-vector-conversions"
-    cflags += " -I#{Formula["openblas"].opt_include}" if build.with? "openblas"
-    args = ["CFLAGS=#{cflags}", "ZLIB=-L#{Formula["zlib"].opt_lib} -lz"]
-    args << "BLASFLAGS=-L#{Formula["openblas"].opt_lib} -lopenblas" if build.with? "openblas"
-    system "make", "plink", *args
-    bin.install "plink" => "plink2"
+    Dir.chdir("1.9") do
+      mv "Makefile.std", "Makefile"
+      ln_s Formula["zlib"].opt_include, "../zlib-1.2.11"
+      cflags = "-Wall -O2 -flax-vector-conversions"
+      cflags += " -I#{Formula["openblas"].opt_include}" if build.with? "openblas"
+      args = ["CFLAGS=#{cflags}", "ZLIB=-L#{Formula["zlib"].opt_lib} -lz"]
+      args << "BLASFLAGS=-L#{Formula["openblas"].opt_lib} -lopenblas" if build.with? "openblas"
+      system "make", "plink", *args
+      bin.install "plink" => "plink2"
+    end
   end
 
   test do
