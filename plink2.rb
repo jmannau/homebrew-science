bbf319c23b43147a5f5194ac380f0db1205e80e3
James Manners <james@manners.net.au>
2017-06-06 10:39:54 +1000

    Updated formula to remove stable block and limit dependancy block for linux builds.

--- plink2.rb
+++ plink2.rb
@@ -1,14 +1,11 @@
 class Plink2 < Formula
   desc "Free, open-source whole genome association analysis toolset."
   homepage "https://www.cog-genomics.org/plink2"
+  url "https://github.com/chrchang/plink-ng/archive/v1.90b4.tar.gz"
+  version "1.90b4"
+  sha256 "2ab909fc6c04062be56be86cf00ad6b3927316a34ae4ceb5fc4c250074cb3d26"
   # doi "10.1186/s13742-015-0047-8"
   # tag "bioinformatics"
-
-  stable do
-    version "1.90b4"
-    url "https://github.com/chrchang/plink-ng/archive/v1.90b4.tar.gz"
-    sha256 "2ab909fc6c04062be56be86cf00ad6b3927316a34ae4ceb5fc4c250074cb3d26"
-  end
 
   bottle do
     cellar :any
@@ -23,7 +20,7 @@ class Plink2 < Formula
   end
 
   depends_on :fortran
-  depends_on "openblas" => :optional
+  depends_on "openblas" => :optional unless OS.mac?
   depends_on "zlib"
 
   def install
